82e311651ec9b1c54aa7b3a0f74de186f44fbcb0
Marco Chini <marco.chini@chini.it>
2024-12-03 22:41:57 +0100

    Metodo per calcolo durata prossimi stint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { collection, collectionData, Firestore } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 import { AsyncPipe } from "@angular/common";
+import { RaceManagerService } from './service/race-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { AsyncPipe } from "@angular/common";
 export class AppComponent {
   firestore: Firestore = inject(Firestore);
   items$: Observable<any[]>;
+  raceManager: RaceManagerService = inject(RaceManagerService);
 
   constructor() {
     const aCollection = collection(this.firestore, 'items')
