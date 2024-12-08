81022e5da8cc7f72141c582819d65fddcdd2f62a
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-08 18:50:14 +0100

    feat: add battery component with dynamic charge level indicator

---
+++ src/app/component/battery/battery.component.ts
@@ -0,0 +1,56 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: 'app-battery',
+  standalone: true,
+  imports: [
+    NgClass
+  ],
+  templateUrl: './battery.component.html',
+  styleUrl: './battery.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BatteryComponent implements OnChanges{
+  @Input() charge: number = 100;
+
+  batteryColor = 'bg-green-500'
+  batteryBorderColor = 'border-gray-200'
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.batteryColor = this.getBackgroundColor(this.charge);
+    this.batteryBorderColor = this.getBorderColor(this.charge);
+  }
+
+  private getBackgroundColor(charge: number) {
+    if (this.charge <= 10) {
+      return 'bg-red-500';
+    }
+    if (this.charge <= 25) {
+      return 'bg-orange-500';
+    }
+    if (this.charge <= 50) {
+      return 'bg-yellow-500';
+    }
+    return 'bg-green-500';
+  }
+
+  private getBorderColor(charge: number) {
+    if (this.charge <= 10) {
+      return 'border-red-500';
+    }
+    if (this.charge <= 25) {
+      return 'border-orange-500';
+    }
+    return 'border-gray-200';
+  }
+
+
+}
