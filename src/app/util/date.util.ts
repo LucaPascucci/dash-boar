d2e84fb844a8492e4e7f7881cbb6c33069bb638e
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-08 01:14:21 +0100

    feat: add date utility functions for countdown formatting

---
+++ src/app/util/date.util.ts
@@ -0,0 +1,27 @@
+import { intervalToDuration } from "date-fns";
+
+export function calculateCountdownStringToDate(futureDate: Date): string {
+  const duration = intervalToDuration({
+    end: futureDate,
+    start: new Date()
+  });
+  if (duration.hours && duration.hours < 0) {
+    return '00:00:00';
+  }
+  if (duration.minutes && duration.minutes < 0) {
+    return '00:00:00';
+  }
+  if (duration.seconds && duration.seconds < 0) {
+    return '00:00:00';
+  }
+  return [padTwo(duration.hours), padTwo(duration.minutes), padTwo(duration.seconds)].join(":");
+}
+
+function padTwo(num: number | undefined) {
+  if (num) {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
+  return '';
+}
+
+
