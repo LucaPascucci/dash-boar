8e2d7d57b0b3fcfa7e1bcc4a635ae0eed904744c
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-13 09:15:06 +0100

    refactor: migrate fuel component to use dedicated fuel service

--- src/app/component/fuel/fuel.component.ts
+++ src/app/component/fuel/fuel.component.ts
@@ -1,15 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, Signal } from '@angular/core';
 import { DatePipe, NgClass } from "@angular/common";
-import { RaceService } from "../../service/race.service";
 import { RaceConfigService } from "../../service/race-config.service";
-import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
-import { PitService } from "../../service/pit.service";
-import { combineLatest, interval, map } from "rxjs";
-import { addMinutes, differenceInMilliseconds, differenceInMinutes, } from "date-fns";
 import { getElapsedTime, getTimeUntilFutureDate } from "../../util/date.util";
-import { Pit } from "../../model/pit";
 import { BatteryComponent } from "../battery/battery.component";
 import { FormsModule } from "@angular/forms";
+import { FuelService } from "../../service/fuel.service";
 
 @Component({
   selector: 'app-fuel',
@@ -19,112 +14,51 @@ import { FormsModule } from "@angular/forms";
   styleUrl: './fuel.component.css'
 })
 export class FuelComponent {
-  private readonly raceService = inject(RaceService);
-  private readonly pitService = inject(PitService);
   private readonly raceConfigService = inject(RaceConfigService);
-  private referenceLapTimeMillisecond = signal(64567); // TODO: collegare vero valore
-  private activeRace = this.raceService.activeRace;
+  private readonly fuelService = inject(FuelService);
 
-  fuelDurationMinute = this.raceConfigService.get().fuelDurationMinute;
+  readonly remainingFuelPercentage = this.fuelService.remainingFuelPercentage;
+  readonly remainingFuelLap = this.fuelService.remainingFuelLap;
+  readonly emptyFuelTime: Signal<Date | undefined> = this.fuelService.emptyFuelTime;
+  readonly lastRefuelTime: Signal<Date | undefined> = this.fuelService.lastRefuelTime;
+
+  emptyFuelCountdown = computed(() => {
+    return getTimeUntilFutureDate(this.emptyFuelTime())
+  });
+
+  sinceLastRefuel = computed(() => {
+    return getElapsedTime(this.lastRefuelTime(), new Date());
+  });
+
+  warning = computed(() => {
+    return this.remainingFuelPercentage() <= 20;
+  });
+  remainingColor = computed(() => {
+    return this.getRemainingColor(this.remainingFuelPercentage());
+  });
 
   isOpen = true;
-  warning = false;
-  remainingColor = 'bg-green-500';
-  fuelPercentage = 100;
-  remainingLap = 0;
-  emptyTime: Date | undefined;
-  emptyCountdown = '--:--:--';
-  lastRefuelTime: Date | undefined;
-  sinceLastRefuel = '--:--:--';
+  fuelDurationMinute = 0;
 
   constructor() {
-    const pits$ = this.pitService.getRacePits().pipe(takeUntilDestroyed())
-    const interval$ = interval(1000).pipe(takeUntilDestroyed());
-
-    combineLatest([toObservable(this.activeRace), pits$, interval$])
-    .pipe(
-        takeUntilDestroyed(),
-        map(([activeRace, pits]) => {
-          const recentRefuelPit = this.getRecentRefuelPit(pits);
-          return {activeRace, recentRefuelPit};
-        })
-    )
-    .subscribe(({activeRace, recentRefuelPit}) => {
-      let date: Date | undefined = undefined;
-
-      if (recentRefuelPit && recentRefuelPit.exitTime) {
-        date = recentRefuelPit.exitTime.toDate()
-      } else if (activeRace) {
-        date = activeRace.start.toDate();
+    effect(() => {
+      const activeRaceConfig = this.raceConfigService.activeRaceConfig()
+      if (activeRaceConfig) {
+        this.fuelDurationMinute = activeRaceConfig.fuelDurationMinute;
       }
-      this.updateInfoFromDate(date);
-    })
+    });
   }
 
   onSubmit() {
-    console.log('New fuel duration minute:', this.fuelDurationMinute);
+    this.raceConfigService.updateFuelDurationMinute(this.fuelDurationMinute);
   }
 
   increaseFuelDuration() {
-    this.fuelDurationMinute++;
+    this.raceConfigService.updateFuelDurationMinute(this.fuelDurationMinute + 1);
   }
 
   decreaseFuelDuration() {
-    this.fuelDurationMinute--;
-  }
-
-  private updateInfoFromDate(date: Date | undefined) {
-    this.lastRefuelTime = date;
-    this.emptyTime = this.calculateEmptyTime(date);
-    this.emptyCountdown = getTimeUntilFutureDate(this.emptyTime);
-    this.fuelPercentage = this.calculateFuelPercentage(this.emptyTime, this.fuelDurationMinute);
-    this.warning = this.fuelPercentage <= 20;
-    this.remainingColor = this.getRemainingColor(this.fuelPercentage);
-    this.remainingLap = this.calculateRemainingLaps(this.emptyTime, this.referenceLapTimeMillisecond());
-    this.sinceLastRefuel = getElapsedTime(date, new Date());
-  }
-
-  private getRecentRefuelPit(pits: Pit[]): Pit | undefined {
-    const sortedRefuelPits = pits
-    .filter(pit => pit.refuel && pit.exitTime !== undefined)
-    .sort((a, b) => {
-      const exitTimeA = a.exitTime ? a.exitTime.toDate().getTime() : 0;
-      const exitTimeB = b.exitTime ? b.exitTime.toDate().getTime() : 0;
-      return exitTimeB - exitTimeA;
-    });
-
-    if (sortedRefuelPits.length > 0) {
-      return sortedRefuelPits[0];
-    }
-    return undefined;
-  }
-
-  private calculateEmptyTime(date: Date | undefined): Date | undefined {
-    if (date) {
-      return addMinutes(date, this.fuelDurationMinute)
-    } else {
-      return undefined
-    }
-  }
-
-  private calculateRemainingLaps(emptyTime: Date | undefined, referenceLapTimeMillisecond: number | undefined): number {
-    if (emptyTime && referenceLapTimeMillisecond) {
-      const millisecondsUntilEmpty = differenceInMilliseconds(emptyTime, new Date());
-      return Math.floor(millisecondsUntilEmpty / referenceLapTimeMillisecond);
-    } else {
-      return 0;
-    }
-  }
-
-  private calculateFuelPercentage(emptyTime: Date | undefined, fuelDurationMinute: number): number {
-    if (emptyTime) {
-      const remainingMinutes = differenceInMinutes(emptyTime, new Date());
-      const percentage = (remainingMinutes / fuelDurationMinute) * 100;
-      const clampedPercentage = Math.max(0, Math.min(percentage, 100));
-      return parseFloat(clampedPercentage.toFixed(0));
-    } else {
-      return 100;
-    }
+    this.raceConfigService.updateFuelDurationMinute(this.fuelDurationMinute - 1);
   }
 
   private getRemainingColor(fuelPercentage: number): string {
