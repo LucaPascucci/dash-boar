51fd4ea711b7fdfb26ee8bc0b919529e5099ace4
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-13 09:57:24 +0100

    feat: add tyre service with real-time tyre change window tracking

---
+++ src/app/service/tyre.service.ts
@@ -0,0 +1,64 @@
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { RaceService } from "./race.service";
+import { RaceConfigService } from "./race-config.service";
+import { PitService } from "./pit.service";
+import { combineLatest, interval } from "rxjs";
+import { takeUntilDestroyed, toObservable } from "@angular/core/rxjs-interop";
+import { addHours } from "date-fns";
+import { Pit } from "../model/pit";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TyreService {
+  private readonly raceService = inject(RaceService);
+  private readonly raceConfigService = inject(RaceConfigService);
+  private readonly pitService = inject(PitService);
+
+  readonly openingTime: WritableSignal<Date | undefined> = signal(undefined)
+  readonly closingTime: WritableSignal<Date | undefined> = signal(undefined);
+
+  readonly tyreChangeWindowOpen: WritableSignal<boolean> = signal(false);
+  readonly remainingTyreChange: WritableSignal<number> = signal(0);
+
+  constructor() {
+
+    combineLatest({
+      pits: toObservable(this.pitService.pits),
+      activeRace: toObservable(this.raceService.activeRace),
+      raceConfig: toObservable(this.raceConfigService.activeRaceConfig),
+      ping: interval(1000)
+    })
+    .pipe(takeUntilDestroyed())
+    .subscribe(({pits, activeRace, raceConfig}) => {
+      if (activeRace && raceConfig) {
+        this.openingTime.set(addHours(activeRace.start.toDate(), raceConfig.startTyreChangeWindowHour));
+        this.closingTime.set(addHours(activeRace.start.toDate(), raceConfig.endTyreChangeWindowHour));
+        this.tyreChangeWindowOpen.set(this.isTyreChangeWindowOpen(this.openingTime(), this.closingTime()));
+      } else {
+        this.openingTime.set(undefined);
+        this.closingTime.set(undefined);
+        this.tyreChangeWindowOpen.set(false);
+      }
+
+      if (raceConfig) {
+        this.remainingTyreChange.set(this.calculateRemainingTyreChange(pits, raceConfig.minTyreChange));
+      }
+    });
+  }
+
+  private isTyreChangeWindowOpen(openingTime: Date | undefined, closingTime: Date | undefined) {
+    if (openingTime && closingTime) {
+      const now = new Date();
+      return now >= openingTime && now <= closingTime;
+    }
+    return false;
+  }
+
+  private calculateRemainingTyreChange(pits: Pit[], minTyreChange:  number): number {
+    const tyreChangesDone = pits.filter(pit => pit.tyreChange).length;
+    return minTyreChange - tyreChangesDone;
+  }
+}
+
+
