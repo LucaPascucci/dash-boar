7e3a8f5a3e80c0954104e7d78554e7b248ebc925
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-13 10:03:01 +0100

    refactor: migrate driver service to use signal for drivers list

--- src/app/service/driver.service.ts
+++ src/app/service/driver.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 import { collection, collectionData, } from "@angular/fire/firestore";
 import { map, Observable, takeUntil } from "rxjs";
 import { Driver } from "../model/driver";
 import { FirestoreService } from "./firestore.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,15 @@ export class DriverService extends FirestoreService {
   protected collectionPath = '/drivers';
   protected collectionRef = collection(this.firestore, this.collectionPath);
 
+  readonly drivers: WritableSignal<Driver[]> = signal([]);
+
   constructor() {
     super();
-  }
-
-  getAll(): Observable<Driver[]> {
-    return collectionData(this.collectionRef).pipe(
-        takeUntil(this.destroy$),
-        map((drivers: Driver[]) => drivers.filter(driver => !driver.deleted))
-    );
+    this.getAll()
+    .pipe(takeUntilDestroyed())
+    .subscribe(drivers => {
+      this.drivers.set(drivers);
+    })
   }
 
   async create(driver: Driver): Promise<void> {
@@ -33,6 +34,13 @@ export class DriverService extends FirestoreService {
 
   getById(id: string): Promise<Driver | undefined> {
     return this.getDataById(id);
+  }
+
+  private getAll(): Observable<Driver[]> {
+    return collectionData(this.collectionRef).pipe(
+        takeUntil(this.destroy$),
+        map((drivers: Driver[]) => drivers.filter(driver => !driver.deleted))
+    );
   }
 
 }
