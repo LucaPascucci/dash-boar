52dc143883fcb56ec678d1ca641dffca87352fe3
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-14 02:31:48 +0100

    feat: add driver component with time tracking and warnings display

---
+++ src/app/component/driver/driver.component.ts
@@ -0,0 +1,46 @@
+import { Component, computed, inject, Signal } from '@angular/core';
+import { NgClass, NgForOf } from "@angular/common";
+import { DriverService } from "../../service/driver.service";
+import { Driver } from "../../model/driver";
+import { millisecondsToLapString, millisecondsToTimeString } from "../../util/date.util";
+import { StintService } from "../../service/stint.service";
+
+@Component({
+  selector: 'app-driver',
+  standalone: true,
+  imports: [
+    NgClass,
+    NgForOf
+  ],
+  templateUrl: './driver.component.html',
+  styleUrl: './driver.component.css'
+})
+export class DriverComponent {
+  private readonly driverService = inject(DriverService);
+  private readonly stintService = inject(StintService);
+
+  readonly drivers: Signal<Driver[]> = this.driverService.drivers;
+  readonly driversTimeOnTrackWarningCount: Signal<number> = this.driverService.driversTimeOnTrackWarningCount;
+  readonly activeStintDriverId = computed(() => {
+    const activeStint = this.stintService.activeStint();
+    if (activeStint) {
+      return activeStint.driverId;
+    }
+    return undefined;
+  });
+
+
+  isOpen = true;
+
+  getDriverReferenceLapTime(driverId: string): string {
+    return millisecondsToLapString(this.driverService.driversReferenceLapTimeMap().get(driverId));
+  }
+
+  getDriverTimeOnTrack(driverId: string): string {
+    return millisecondsToTimeString(this.driverService.driversTimeOnTrackMap().get(driverId));
+  }
+
+  getDriverTimeOnTrackWarning(driverId: string): boolean {
+    return this.driverService.driversTimeOnTrackWarningMap().get(driverId) || false;
+  }
+}
