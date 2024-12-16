e0ae5d29c596ae9b9d534d407563416351e9e3b8
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-16 23:42:20 +0100

    feat: implement pit table with real-time stats and totals display

--- src/app/component/pit/pit.component.ts
+++ src/app/component/pit/pit.component.ts
@@ -1,16 +1,69 @@
-import { Component } from '@angular/core';
-import { NgClass } from "@angular/common";
+import { Component, computed, inject, Signal } from '@angular/core';
+import { DatePipe, NgClass, NgForOf } from "@angular/common";
+import { PitService } from "../../service/pit.service";
+import { Pit } from "../../model/pit";
+import {
+  getElapsedTime,
+  millisecondsToLapString,
+  millisecondsToTimeString
+} from "../../util/date.util";
 
 @Component({
   selector: 'app-pit',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    DatePipe,
+    NgForOf
   ],
   templateUrl: './pit.component.html',
   styleUrl: './pit.component.css'
 })
 export class PitComponent {
+  private readonly pitService = inject(PitService);
 
-  isOpen = false;
+  readonly pits: Signal<Pit[]> = this.pitService.pits;
+
+  readonly activePitId = computed(() => {
+    const activePit = this.pitService.activePit();
+    if (activePit) {
+      return activePit.id;
+    }
+    return undefined;
+  })
+
+  readonly completedDriverChanges: Signal<number> = this.pitService.completedDriverChanges;
+
+  isOpen = true;
+
+  calculatePitTime(start: Date, end: Date | undefined): string {
+    if (end) {
+      return getElapsedTime(start, end);
+    }
+    return getElapsedTime(start, new Date());
+  }
+
+  calculateTotalPitTime(): string {
+    const pits = this.pitService.pits();
+    let timeSpent = 0;
+    pits.forEach(pit => {
+      if (pit.exitTime) {
+        timeSpent += pit.exitTime.toDate().getTime() - pit.entryTime.toDate().getTime();
+      } else {
+        // PIT CORRENTE
+        timeSpent += new Date().getTime() - pit.entryTime.toDate().getTime();
+      }
+    })
+    return millisecondsToTimeString(timeSpent);
+  }
+
+  calculateTotalRefueling(): number {
+    const pits = this.pitService.pits();
+    return pits.filter(pit => pit.exitTime && pit.refuel).length;
+  }
+
+  calculateTotalTyreChanges(): number {
+    const pits = this.pitService.pits();
+    return pits.filter(pit => pit.exitTime && pit.tyreChange).length;
+  }
 }
