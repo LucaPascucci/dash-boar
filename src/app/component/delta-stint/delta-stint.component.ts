55764b7262718eb95f8314128adf098dc9b67145
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-18 10:26:27 +0100

    feat: add delta stint component with real-time timing comparison display

---
+++ src/app/component/delta-stint/delta-stint.component.ts
@@ -0,0 +1,37 @@
+import { Component, computed, inject, Signal } from '@angular/core';
+import { NgClass, NgForOf } from "@angular/common";
+import { DeltaStintService } from "../../service/delta-stint.service";
+import { DeltaStint } from "../../model/delta-stint";
+import { StintService } from "../../service/stint.service";
+import { millisecondsToTimeString } from "../../util/date.util";
+
+@Component({
+  selector: 'app-delta-stint',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgClass
+  ],
+  templateUrl: './delta-stint.component.html',
+  styleUrl: './delta-stint.component.css'
+})
+export class DeltaStintComponent {
+  private readonly stintService = inject(StintService);
+  private readonly deltaStintService = inject(DeltaStintService);
+
+  readonly deltaStints: Signal<DeltaStint[]> = this.deltaStintService.deltaStints;
+  readonly totalDeltaMilliseconds: Signal<number> = this.deltaStintService.totalDeltaMilliseconds;
+  readonly activeStintId = computed(() => {
+    const activeStint = this.stintService.activeStint();
+    if (activeStint) {
+      return activeStint.id;
+    }
+    return undefined;
+  })
+
+  isOpen: boolean = true;
+
+  getMillisecondsToTimeString(milliseconds: number): string {
+    return millisecondsToTimeString(Math.abs(milliseconds));
+  }
+}
