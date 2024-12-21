143ff8a38d1fc2288b96d6051c6d09e6cbfadd9f
Luca Pascucci <luca.pascucci@injenia.it>
2024-12-22 00:04:23 +0100

    feat: add timeline step model for race event tracking

---
+++ src/app/model/timeline-step.ts
@@ -0,0 +1,12 @@
+import { Pit } from "./pit";
+import { Stint } from "./stint";
+
+export interface TimelineStep {
+  type: 'STINT' | 'PIT';
+  pit: Pit | undefined;
+  stint: Stint | undefined;
+  start: Date;
+  end: Date | undefined;
+  durationMills:  number | undefined;
+  status: 'DONE' | 'ACTIVE' | 'FUTURE'
+}
