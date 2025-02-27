d638927476d1d4c5192365a1751e92571608b365
Luca Pascucci <luca.pascucci@injenia.it>
2025-02-27 22:28:07 +0100

    feat: add document component with PDF file list

---
+++ src/app/component/document/document.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgClass, NgForOf } from "@angular/common";
+
+@Component({
+  selector: 'app-document',
+  imports: [
+    NgClass,
+    NgForOf
+  ],
+  templateUrl: './document.component.html',
+  styleUrl: './document.component.css'
+})
+export class DocumentComponent {
+
+  isOpen = true;
+
+  documents = [
+    { name: 'Briefing', url: 'assets/documents/briefing.pdf' },
+    { name: 'Entry list', url: 'assets/documents/entry-list.pdf' },
+    { name: 'Programma Venerdì', url: 'assets/documents/programma-venerdi.pdf' },
+    { name: 'Programma 24H', url: 'assets/documents/programma-24h.pdf' },
+    { name: 'Regolamento 24H', url: 'assets/documents/regolamento-24h.pdf' }
+  ];
+
+  openDocument(url: string) {
+    window.open(url, '_blank');
+  }
+}
